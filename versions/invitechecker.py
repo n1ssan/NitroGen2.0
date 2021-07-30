da546277ec5d633334a5a54b97c8f238ab28345d
m000000000n <80481493+m000000000n@users.noreply.github.com>
2021-07-30 14:31:39 -0700

    Update invitechecker.py

--- versions/invitechecker.py
+++ versions/invitechecker.py
@@ -1,1 +1,11 @@
 
+
+url = "https://discordapp.com/api/v6/invite/"+m
+  r = requests.get(url)
+  if r.status_code == 200:
+    print('discord.gg/'+m+"Is valid!")
+  elif r.status_code == 404:
+    print(f"Invalid {url}")
+  elif r.status_code == 429:
+    print("Ratelimited.")
+    
