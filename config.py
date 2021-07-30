5c19c941edc029ade789a02a86dc448ef90f3c41
m000000000n <80481493+m000000000n@users.noreply.github.com>
2021-07-30 10:15:53 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+lettersOnly=False #change to True if you would like letters only
+numbersOnly=False # change to true if you want numbers only 
+lnmix=True # change to False if you dont want letters and numbers.
