8950b147d5e23aeedb676218f0202884a8dcd873
n1ssan <88170203+n1ssan@users.noreply.github.com>
2021-07-29 17:32:58 -0700

    Update main.py
    You don’t have any proxy’s so I recommend you use this instead @m000000000n

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 # Github.com/m000000000n
 import random
 import requests
-a = ['1','2','3','4','5','6','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
-
+import time
+a = ['1','2','3','4','5','6','a','b','c','d','e','f','g','h','i','j','k',]
+print("Wait ten seconds for codes to start coming  \n | \n V")
 while True:
  w = random.choice(a)
  d = random.choice(a)
  e = random.choice(a)
  f = random.choice(a)
- g = random.choice(a)
- h = random.choice(a)
- i = random.choice(a)
- m = f+e+w+d+g+h+i
+ m = f+e+w+d
  c = 'https://discordapp.com/api/v6/entitlements/gift-codes/'+m+'?with_application=false&with_subscription_plan=true'
-
+ time.sleep(10)
  b = requests.get(c)
  if "Unknown Gift Code" in b.text:
 
     print('\033[31m' + 'discord.gifts/'+m+' Was invalid'+ '\033[0m')
- if b.status_code == 429:
+ elif b.status_code == 429:
    print("\033[1;33m"+"Rate Limited"+"\033[0m")
 
- if "Unknown Gift Code" and "You are being rate limited" not in b.text:
+ else:
    print('discord.gifts/'+m+'Is Valid!')
    file = open("okcodes.txt", "w")
    file.write('discord.gifts/'+m)
