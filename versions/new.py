e8171cf91f6d09ffdb247cc70d6952f33f68239a
m000000000n <80481493+m000000000n@users.noreply.github.com>
2021-07-30 14:09:45 -0700

    Update new.py

--- versions/new.py
+++ versions/new.py
@@ -9,7 +9,7 @@ if numbersOnly == True:
   a=['1','2','3','4','5','6','7','8','9','0']
 if lnmix == True:
   a = ['1','2','3','4','5','6','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','7','8','9','0']
-'https://discordapp.com/api/v6/entitlements/gift-codes/'+m+'/redeem', headers={'authorization': token, 'user-agent': 'Mozilla/5.0'}
+
                                                                                     
 while True:                                                                     
    w = random.choice(a)                                                           
