3c1c1b1091cb0617664680661ab006c72d06a223
m000000000n <80481493+m000000000n@users.noreply.github.com>
2021-07-31 08:53:13 -0700

    Update config.py

--- versions/config.py
+++ versions/config.py
@@ -2,12 +2,12 @@
 ##############################################################################################################
 ##############################################################################################################
 ##############################################################################################################
-lettersOnly=False #change to True if you would like letters only
-numbersOnly=False # change to true if you want numbers only 
-lnmix=True # change to False if you dont want letters and numbers.
-sniperOn=False # Change to true if you want to auto claim the codes (you need your token for this.)
+lettersOnly=False #change to True if you would like letters only#############################################
+numbersOnly=False # change to true if you want numbers only##################################################
+lnmix=True # change to False if you dont want letters and numbers.###########################################
+sniperOn=False # Change to true if you want to auto claim the codes (you need your token for this.)##########
 #if sniperOn=False then ignore this otherwise enter your discord token in the token = '' dont know how to get your token? look it up online, Plenty of guides.
-token = '' # Assign token var to your token.
+token = '' # Assign token var to your token. ################################################################
 ##############################################################################################################
 ##############################################################################################################
 ##############################################################################################################
