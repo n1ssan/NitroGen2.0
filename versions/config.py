881486c8907a45ca883e7f05ee0033d6d8351575
m000000000n <80481493+m000000000n@users.noreply.github.com>
2021-07-31 12:52:12 -0700

    Update config.py

--- versions/config.py
+++ versions/config.py
@@ -7,7 +7,9 @@ numbersOnly=False # change to true if you want numbers only#####################
 lnmix=True # change to False if you dont want letters and numbers.###########################################
 sniperOn=False # Change to true if you want to auto claim the codes (you need your token for this.)##########
 #if sniperOn=False then ignore this otherwise enter your discord token in the token = '' dont know how to get your token? look it up online, Plenty of guides.
-token = '' # Assign token var to your token. ################################################################
+token = '' # Assign token var to your token.#################################################################
+altokenIsToken=False#SET TRUE IF YOU WANT TO SNIPE NITRO ON A ALT ACCOUNT####################################
+#############################################################################################################
 ##############################################################################################################
 ##############################################################################################################
 ##############################################################################################################
