e0e7b6662d621d728fb06086d41dfb09f46014dc
m000000000n <80481493+m000000000n@users.noreply.github.com>
2021-07-31 20:48:03 -0700

    Update new.py

--- versions/new.py
+++ versions/new.py
@@ -50,7 +50,7 @@ if choice == "1":
                                                                                   
     print(Fore.WHITE+Back.RED +'[INVAILD]'+Style.RESET_ALL+'\033[31m' + '[x]: discord.gifts/'+m+' Was invalid'+ '\033[0m')            
    if b.status_code == 429:                                                       
-    print(Fore.WHITE+Back.YELLOW+"[WARNING]"+Style.RESET_ALL+"\033[1;33m"+"[-]: Rate Limited"+"\033[0m")                                 
+    print(Fore.WHITE+Back.YELLOW+"[WARNING]"+Style.RESET_ALL+"\033[1;33m"+"[-]: Rate Limited | Waiting 40 Seconds|"+"\033[0m")                                 
                                                                                    
    if b.status_code == 200:                                                       
     print('[GOOD] [+]: discord.gifts/'+m+'Is Valid!')
